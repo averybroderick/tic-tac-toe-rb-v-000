1b77a1baa4038f3701f401adc4e7accb7bb91ec1
Avery Broderick <avery.c.broderick@gmail.com>
2017-12-06 14:39:00 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,18 @@
+WIN_COMBINATIONS = [
+  [0,1,2], #horizontal
+  [3,4,5], #horizontal
+  [6,7,8], #horizontal
+  [0,3,6], #vertical
+  [1,4,7], #vertical
+  [2,5,8], #vertical
+  [0,4,8], #diagonal
+  [2,4,6] #diagonal
+]
 
+def display_board(board)
+  puts " #{board[0]} " + "|" + " #{board[1]} " + "|" + " #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} " + "|" + " #{board[4]} " + "|" + " #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} " + "|" + " #{board[7]} " + "|" + " #{board[8]} "
+end

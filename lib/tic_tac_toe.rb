74fdf7c23cf33419c95054f0dc9dfd5f4e8f4280
Avery Broderick <avery.c.broderick@gmail.com>
2017-12-06 15:07:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -90,8 +90,5 @@ def play(board)
   until over?(board)
     turn(board)
   end
-  if won?(board)
-    puts "Congratulations #{winner(board)}!"
-  elsif draw?(board)
-    puts "Cat's Game!"
+  won?(board)? puts "Congratulations #{winner(board)}!" : puts "Cat's Game!"
 end
